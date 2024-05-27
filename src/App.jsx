9365be7d8a94c781aa9fa6f2df5133618c96b04d
Aman Kumar <amanksh05@gmail.com>
2024-05-28 01:16:03 +0530

    AppWrite ids are configured

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h3>
+        Blog App with apprite.
+      </h3>
+    </>
+  )
+}
+
+export default App
