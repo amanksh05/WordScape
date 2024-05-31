4e321355693deb1f0be6139b86baeb162c0e03ca
Aman Kumar <amanksh05@gmail.com>
2024-06-01 01:58:09 +0530

    Redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appWrite/auth'
+import { login,logout } from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-      <h3>
-        Blog App with apprite.
-      </h3>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout)
+        }
+      })
+      .finally(()=>{setLoading(false)})
+  }, [])
+
+  return !loading?(
+    <div className='min-h-screen'></div>
+  ): null
 }
 
 export default App
